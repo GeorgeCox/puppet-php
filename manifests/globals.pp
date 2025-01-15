2d2ca0c7a20e3c014419a806544519c927d3a743
George Cox <George.Cox@fasthosts.com>
2025-01-15 14:30:47 +0000

    update

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -51,151 +51,153 @@ class php::globals (
     $globals_php_version = $php_version
   }
 
-  case $facts['os']['family'] {
-    'Debian': {
-      if $facts['os']['name'] == 'Ubuntu' {
-        case $globals_php_version {
-          /^[578].[0-9]/: {
-            case $flavor {
-              'zend': {
-                $default_config_root = "/etc/php/${globals_php_version}-zend"
-                $fpm_service_name = "php${globals_php_version}-zend-fpm"
-                $package_prefix = "php${globals_php_version}-zend-"
+  $php_version.each | $version | {
+    case $facts['os']['family'] {
+      'Debian': {
+        if $facts['os']['name'] == 'Ubuntu' {
+          case php_version {
+            /^[578].[0-9]/: {
+              case $flavor {
+                'zend': {
+                  $default_config_root = "/etc/php/${globals_php_version}-zend"
+                  $fpm_service_name = "php${globals_php_version}-zend-fpm"
+                  $package_prefix = "php${globals_php_version}-zend-"
+                }
+                default: {
+                  $default_config_root = "/etc/php/${globals_php_version}"
+                  $fpm_service_name = "php${globals_php_version}-fpm"
+                  $package_prefix = "php${globals_php_version}-"
+                }
               }
-              default: {
-                $default_config_root = "/etc/php/${globals_php_version}"
-                $fpm_service_name = "php${globals_php_version}-fpm"
-                $package_prefix = "php${globals_php_version}-"
-              }
-            }
-            $default_fpm_pid_file = "/var/run/php/php${globals_php_version}-fpm.pid"
-            $fpm_error_log = "/var/log/php${globals_php_version}-fpm.log"
-            $ext_tool_enable = "/usr/sbin/phpenmod -v ${globals_php_version}"
-            $ext_tool_query = "/usr/sbin/phpquery -v ${globals_php_version}"
-          }
-          default: {
-            # Default php installation from Ubuntu official repository use the following paths until 16.04
-            # For PPA please use the $php_version to override it.
-            $default_config_root = '/etc/php5'
-            $default_fpm_pid_file = '/var/run/php5-fpm.pid'
-            $fpm_error_log = '/var/log/php5-fpm.log'
-            $fpm_service_name = 'php5-fpm'
-            $ext_tool_enable = '/usr/sbin/php5enmod'
-            $ext_tool_query = '/usr/sbin/php5query'
-            $package_prefix = 'php5-'
-          }
-        }
-      } else {
-        if $flavor == 'zend' and versioncmp($facts['os']['release']['major'], '11') > 0 {
-          fail("Zend PHP is unsupported on this Debian release: ${fact('os.release.major')}")
-        }
-        case $globals_php_version {
-          /^5\.6/,
-          /^7\.[0-9]/,
-          /^8\.[0-9]/: {
-            case $flavor {
-              'zend': {
-                $default_config_root = "/etc/php/${globals_php_version}-zend"
-                $fpm_service_name = "php${globals_php_version}-zend-fpm"
-                $package_prefix = "php${globals_php_version}-zend-"
-              }
-              default: {
-                $default_config_root = "/etc/php/${globals_php_version}"
-                $fpm_service_name = "php${globals_php_version}-fpm"
-                $package_prefix = "php${globals_php_version}-"
-              }
-            }
-            $default_fpm_pid_file = "/var/run/php/php${globals_php_version}-fpm.pid"
-            $fpm_error_log        = "/var/log/php${globals_php_version}-fpm.log"
-            $ext_tool_enable      = "/usr/sbin/phpenmod -v ${globals_php_version}"
-            $ext_tool_query       = "/usr/sbin/phpquery -v ${globals_php_version}"
-          }
-          default: {
-            $default_config_root = '/etc/php5'
-            $default_fpm_pid_file = '/var/run/php5-fpm.pid'
-            $fpm_error_log = '/var/log/php5-fpm.log'
-            $fpm_service_name = 'php5-fpm'
-            $ext_tool_enable = '/usr/sbin/php5enmod'
-            $ext_tool_query = '/usr/sbin/php5query'
-            $package_prefix = 'php5-'
-          }
-        }
-      }
-    }
-    'Suse': {
-      case $globals_php_version {
-        /^7/: {
-          $default_config_root = '/etc/php7'
-          $package_prefix = 'php7-'
-          $default_fpm_pid_file = '/var/run/php7-fpm.pid'
-          $fpm_error_log = '/var/log/php7-fpm.log'
-        }
-        default: {
-          $default_config_root = '/etc/php5'
-          $package_prefix = 'php5-'
-          $default_fpm_pid_file = '/var/run/php5-fpm.pid'
-          $fpm_error_log = '/var/log/php5-fpm.log'
-        }
-      }
-    }
-    'RedHat': {
-      case $flavor {
-        'zend': {
-          $php_version_sans_dot = $php_version.regsubst(/\./, '', 'G')
-          $default_config_root    = "/etc/opt/zend/php${php_version_sans_dot}zend"
-          $default_fpm_pid_file   = '/var/run/php-fpm/php-fpm.pid'
-          $fpm_service_name       = "php${php_version_sans_dot}zend-php-fpm"
-          $package_prefix         = "php${php_version_sans_dot}zend-php-"
-        }
-
-        default: {
-          case $rhscl_mode {
-            'remi': {
-              $rhscl_root             = "/opt/remi/${php_version}/root"
-              $default_config_root    = "/etc/opt/remi/${php_version}"
-              $default_fpm_pid_file   = '/var/run/php-fpm/php-fpm.pid'
-              $package_prefix         = "${php_version}-php-"
-              $fpm_service_name       = "${php_version}-php-fpm"
-            }
-            'rhscl': {
-              $rhscl_root             = "/opt/rh/${php_version}/root"
-              $default_config_root    = "/etc/opt/rh/${php_version}" # rhscl registers contents by copy in /etc/opt/rh
-              $default_fpm_pid_file   = "/var/opt/rh/${php_version}/run/php-fpm/php-fpm.pid"
-              $package_prefix         = "${php_version}-php-"
-              $fpm_service_name       = "${php_version}-php-fpm"
-            }
-            undef: {
-              $default_config_root    = '/etc/php.d'
-              $default_fpm_pid_file   = '/var/run/php-fpm/php-fpm.pid'
-              $fpm_service_name       = undef
-              $package_prefix         = undef
+              $default_fpm_pid_file = "/var/run/php/php${globals_php_version}-fpm.pid"
+              $fpm_error_log = "/var/log/php${globals_php_version}-fpm.log"
+              $ext_tool_enable = "/usr/sbin/phpenmod -v ${globals_php_version}"
+              $ext_tool_query = "/usr/sbin/phpquery -v ${globals_php_version}"
             }
             default: {
-              fail("Unsupported rhscl_mode '${rhscl_mode}'")
+              # Default php installation from Ubuntu official repository use the following paths until 16.04
+              # For PPA please use the $php_version to override it.
+              $default_config_root = '/etc/php5'
+              $default_fpm_pid_file = '/var/run/php5-fpm.pid'
+              $fpm_error_log = '/var/log/php5-fpm.log'
+              $fpm_service_name = 'php5-fpm'
+              $ext_tool_enable = '/usr/sbin/php5enmod'
+              $ext_tool_query = '/usr/sbin/php5query'
+              $package_prefix = 'php5-'
+            }
+          }
+        } else {
+          if $flavor == 'zend' and versioncmp($facts['os']['release']['major'], '11') > 0 {
+            fail("Zend PHP is unsupported on this Debian release: ${fact('os.release.major')}")
+          }
+          case php_version {
+            /^5\.6/,
+            /^7\.[0-9]/,
+            /^8\.[0-9]/: {
+              case $flavor {
+                'zend': {
+                  $default_config_root = "/etc/php/${globals_php_version}-zend"
+                  $fpm_service_name = "php${globals_php_version}-zend-fpm"
+                  $package_prefix = "php${globals_php_version}-zend-"
+                }
+                default: {
+                  $default_config_root = "/etc/php/${globals_php_version}"
+                  $fpm_service_name = "php${globals_php_version}-fpm"
+                  $package_prefix = "php${globals_php_version}-"
+                }
+              }
+              $default_fpm_pid_file = "/var/run/php/php${globals_php_version}-fpm.pid"
+              $fpm_error_log        = "/var/log/php${globals_php_version}-fpm.log"
+              $ext_tool_enable      = "/usr/sbin/phpenmod -v ${globals_php_version}"
+              $ext_tool_query       = "/usr/sbin/phpquery -v ${globals_php_version}"
+            }
+            default: {
+              $default_config_root = '/etc/php5'
+              $default_fpm_pid_file = '/var/run/php5-fpm.pid'
+              $fpm_error_log = '/var/log/php5-fpm.log'
+              $fpm_service_name = 'php5-fpm'
+              $ext_tool_enable = '/usr/sbin/php5enmod'
+              $ext_tool_query = '/usr/sbin/php5query'
+              $package_prefix = 'php5-'
             }
           }
         }
       }
-    }
-    'FreeBSD': {
-      case $globals_php_version {
-        /^(\d)\.(\d)$/: {
-          $package_prefix = "php${1}${2}-"
-        }
-        default: {
-          $package_prefix = 'php56-'
+      'Suse': {
+        case php_version {
+          /^7/: {
+            $default_config_root = '/etc/php7'
+            $package_prefix = 'php7-'
+            $default_fpm_pid_file = '/var/run/php7-fpm.pid'
+            $fpm_error_log = '/var/log/php7-fpm.log'
+          }
+          default: {
+            $default_config_root = '/etc/php5'
+            $package_prefix = 'php5-'
+            $default_fpm_pid_file = '/var/run/php5-fpm.pid'
+            $fpm_error_log = '/var/log/php5-fpm.log'
+          }
         }
       }
-      $default_config_root  = '/usr/local/etc'
-      $default_fpm_pid_file = '/var/run/php-fpm.pid'
-      $fpm_service_name     = undef
-    }
-    'Archlinux': {
-      $default_config_root  =  '/etc/php'
-      $default_fpm_pid_file = '/run/php-fpm/php-fpm.pid'
-    }
-    default: {
-      fail("Unsupported osfamily: ${facts['os']['family']}")
+      'RedHat': {
+        case $flavor {
+          'zend': {
+            $php_version_sans_dot = $php_version.regsubst(/\./, '', 'G')
+            $default_config_root    = "/etc/opt/zend/php${php_version_sans_dot}zend"
+            $default_fpm_pid_file   = '/var/run/php-fpm/php-fpm.pid'
+            $fpm_service_name       = "php${php_version_sans_dot}zend-php-fpm"
+            $package_prefix         = "php${php_version_sans_dot}zend-php-"
+          }
+  
+          default: {
+            case $rhscl_mode {
+              'remi': {
+                $rhscl_root             = "/opt/remi/${php_version}/root"
+                $default_config_root    = "/etc/opt/remi/${php_version}"
+                $default_fpm_pid_file   = '/var/run/php-fpm/php-fpm.pid'
+                $package_prefix         = "${php_version}-php-"
+                $fpm_service_name       = "${php_version}-php-fpm"
+              }
+              'rhscl': {
+                $rhscl_root             = "/opt/rh/${php_version}/root"
+                $default_config_root    = "/etc/opt/rh/${php_version}" # rhscl registers contents by copy in /etc/opt/rh
+                $default_fpm_pid_file   = "/var/opt/rh/${php_version}/run/php-fpm/php-fpm.pid"
+                $package_prefix         = "${php_version}-php-"
+                $fpm_service_name       = "${php_version}-php-fpm"
+              }
+              undef: {
+                $default_config_root    = '/etc/php.d'
+                $default_fpm_pid_file   = '/var/run/php-fpm/php-fpm.pid'
+                $fpm_service_name       = undef
+                $package_prefix         = undef
+              }
+              default: {
+                fail("Unsupported rhscl_mode '${rhscl_mode}'")
+              }
+            }
+          }
+        }
+      }
+      'FreeBSD': {
+        case php_version {
+          /^(\d)\.(\d)$/: {
+            $package_prefix = "php${1}${2}-"
+          }
+          default: {
+            $package_prefix = 'php56-'
+          }
+        }
+        $default_config_root  = '/usr/local/etc'
+        $default_fpm_pid_file = '/var/run/php-fpm.pid'
+        $fpm_service_name     = undef
+      }
+      'Archlinux': {
+        $default_config_root  =  '/etc/php'
+        $default_fpm_pid_file = '/run/php-fpm/php-fpm.pid'
+      }
+      default: {
+        fail("Unsupported osfamily: ${facts['os']['family']}")
+      }
     }
   }
 
